79cbd8793a22da7802b2e6a63c71d8a5aaec86ce
nshava67 <nshava17@gmail.com>
2026-02-26 15:19:36 +0400

    commit

---
+++ level 03/cw .py
@@ -0,0 +1,18 @@
+#ცვლადი არის მონაცემთა შესანახი დასახელებული ადგილი მეხსიერებაში,რომელშც ინახება ინფორმაცია!
+film="home alone"
+print(film)
+#კონკატინაცია
+print(film * 20)
+print("nikita shavidze" * 10)
+name="nikita"
+surname="shavidze"
+fullname="nikita shavidze"
+print(fullname)
+
+name="nikita" 
+age=15
+height=1.69
+
+print(name)
+print(age)
+print(height)
---
+++ level 03/hw.py
@@ -0,0 +1,20 @@
+#ცვლადი არის შესანახი ადგილი,იგივე "ყუთი" რომელშიც ინახება მონაცემი მაგ:რიცხვი,სახელი,საჭირო ინფორმაცია და სხვა
+age=14
+
+#my name,height and age
+name="nikita"
+height='1.68'
+age='14'
+
+sentence="my name is"+name+"my height is"+height+"my age is "+age
+print(sentence)
+
+name="nikita "
+surname="shavidze"
+fullname=name+surname
+print(fullname)
+
+level = 2
+level = 3
+
+print(level)

1d55047dcd3b7abd88d5103a74ab5cc6faf09c29
nshava67 <nshava17@gmail.com>
2026-03-12 18:37:40 +0400

    commit

---
+++ level 05/cw .py
@@ -0,0 +1,15 @@
+#Debugging არის პროგრამირებაში დაშვებული შეცდომების მოძებნას და გასწორებას
+
+my_name="nikita"
+my_age=15
+
+#მესამე სავარჯიშოში სწორი პასუხია 1
+#მეოთხე სავარჯიშოში გამოიტანს errorს
+
+name="lika"
+print(name)
+
+first_user="luka"
+nduser="giorgi1"
+print(first_user)
+

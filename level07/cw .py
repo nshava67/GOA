e73c02dcba299793bb1b4a35ddf7cfb9dafc56f7
nshava67 <nshava17@gmail.com>
2026-03-26 00:16:44 +0400

    commit

---
+++ level07/cw .py
@@ -0,0 +1,32 @@
+#კონკატინაცია არის ორი სტრინგის შეერთება 
+#მაგ:
+# str
+print("Hello")
+
+# int
+print(10)
+
+# float
+print(3.14)
+
+"10"+"23"=="1023"
+
+age=int(input("შეიტანეთ რიცხი:"))
+print(age + 10)
+#დავუმათეთ int
+print(int("20") + int("5"))
+
+
+
+first= input("შეიყვანე შენი სახელი: ")
+second= input("შეიყვანე შენი გვარი: ")
+
+print(first+""+second)
+
+
+
+
+
+one= int(input("აქ შეიყვანეთ პირველი რიცხვი"))
+two= int(input("აქ შეითანეთ მეორე რიცხვი"))
+print("ნამრვლი:", one * two)

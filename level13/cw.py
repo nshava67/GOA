70c5b64d09ee6ef024eb24e1441e9c840d289b51
nshava67 <nshava17@gmail.com>
2026-05-16 00:05:24 +0400

    commit

---
+++ level13/cw.py
@@ -0,0 +1,24 @@
+#algorithm არის კონკრეტული დავალების შესასრულებლად გაწერილი ნავიჯები ზუსტი თანმიმდევრობით(მაგ ჩაის მომზადება,ხაჭაპურის ტგამოცხობა,და რიცხვების შეკრება)
+
+#control flow არის შესრულების ხერხი(sequence,iteration,selection)
+
+#flow chart არის ფოტოს საშუალებით ვიზუალურად ალგორითმის გამოსახვა
+#pseudocode არის ალგორითმის არწერის ერთ ერთი ფორმა,რომელიც ჰგავს პროგრამირების კოდს.
+#natural language არის ჩვეულებრივი სალაპარაკო ენით ახსნილი ალგორითმი
+
+
+for i in range(1,25):
+  print(i)
+
+for i in range(2,46,2):
+  print(i)
+
+for i in range(100):
+  print("nikita")
+
+
+hello=int(input("enter your first number:"))
+hello2=int(input("enter your secod number:"))
+
+for i in range(hello+hello2,1):
+  print(i)
---
+++ level13/hw .py
@@ -0,0 +1,11 @@
+for i in range(1,21):
+  print(i)
+
+for i in range(2,32,2):
+  print(i)
+
+for i in range(1,11):
+  print (i)
+
+for i in range(15):
+  print("*")

2edc5c42970d937e3fb49f9bda985d93b5ad7360
nshava67 <nshava17@gmail.com>
2026-06-12 18:59:35 +0400

    second commit

---
+++ level 17/cw.py
@@ -0,0 +1,14 @@
+email=input("enter your email:")
+password=input("enter your password:")
+
+userr_email="nikita@gmail.com"
+userr_password="677667"
+
+
+while email !=""  and password !="" and email==userr_email and password==userr_password:
+  print("welvome!")
+
+
+for i in range(10):
+  print("group 91 is best")
+
---
+++ level 17/hw.py
@@ -0,0 +1,29 @@
+for i in range(20):
+  print("nikita")
+
+
+number=0
+
+while number < 20:
+  print("nikita")
+
+
+number1=int(input("enter your first number:"))
+number2=int(input("enter your second number:"))
+
+for i in range(number1,number2+1):
+  print(i)
+
+
+
+number1=int(input("enter your first number:"))
+number2=int(input("enter your second number:"))
+
+
+while number1 != number2:
+  print("the numbers are not correct")
+
+  number1=int(input("enter your first number:"))
+  number2=int(input("enter your second number:"))
+
+print("numbers are correct!")
